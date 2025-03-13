5d8c6962ec010c3f91c9f9f41e732bc96349ea75
Avizit Roy <contact@avizitrx.com>
2025-03-13 20:19:19 +0600

    ui: change hero section text color to gradient

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
           <BlurFade delay={0.25 * 2} inView>
             <h1 className="text-center md:text-start pt-5 text-3xl md:text-6xl font-semibold font-stretch-extra-expanded">
               <span className="text-neutral-500">I&apos;m a</span>{" "}
-              <span className="text-amber-600">
+              <span className="bg-gradient-to-r from-[#FF0080] via-[#7928CA] to-[#0070F3] text-transparent bg-clip-text">
                 AI/ML, Web & Mobile App Developer.
               </span>
             </h1>
