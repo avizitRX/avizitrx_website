93c9fc579c42c91a5b281839b6513c9512e277a3
Avizit Roy <contact@avizitrx.com>
2025-03-20 11:41:14 +0600

    hero section update for speed optimization

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { BlurFade } from "./magicui/blur-fade";
 import { InteractiveHoverButton } from "./magicui/interactive-hover-button";
-import { TextAnimate } from "./magicui/text-animate";
 import Image from "next/image";
 import avizitRoy from "../public/Avizit Roy.png";
 import Link from "next/link";
@@ -15,59 +13,42 @@ const HeroSection = () => {
         <div className="container">
           {/* Hero Text for Mobile*/}
           <div className="md:hidden">
-            <BlurFade delay={0.25} inView>
-              <h1 className="text-4xl md:text-6xl font-semibold flex justify-center">
-                Hi, I&apos;m Avizit Roy!
-              </h1>
-            </BlurFade>
+            <h1 className="text-4xl md:text-6xl font-semibold flex justify-center">
+              Hi, I&apos;m Avizit Roy!
+            </h1>
           </div>
 
           {/* Hero Text */}
           <div className="hidden md:block">
-            <BlurFade delay={0.25} inView>
-              <h1 className="text-6xl font-semibold flex items-center gap-3">
-                Hi, I&apos;m
-                <Image
-                  src={avizitRoy}
-                  alt="Avizit Roy — avizitRX"
-                  height={80}
-                  width={80}
-                  priority
-                  className="inline-block outline-4 outline-white outline-solid outline-offset-0 shadow-2xl rounded-3xl"
-                />
-                <span>Avizit Roy!</span>
-              </h1>
-            </BlurFade>
+            <h1 className="text-6xl font-semibold flex items-center gap-3">
+              Hi, I&apos;m
+              <Image
+                src={avizitRoy}
+                alt="Avizit Roy — avizitRX"
+                height={80}
+                width={80}
+                priority
+                className="inline-block outline-4 outline-white outline-solid outline-offset-0 shadow-2xl rounded-3xl"
+              />
+              <span>Avizit Roy!</span>
+            </h1>
           </div>
 
-          <BlurFade delay={0.25 * 2} inView>
-            <h1 className="text-center md:text-start pt-5 text-3xl md:text-6xl font-semibold font-stretch-extra-expanded">
-              <span className="text-neutral-500">I&apos;m a</span>{" "}
-              <span className="bg-gradient-to-r from-[#FF0080] via-[#7928CA] to-[#0070F3] text-transparent bg-clip-text">
-                AI/ML, Web & Mobile App Developer.
-              </span>
-            </h1>
-          </BlurFade>
+          <h1 className="text-center md:text-start pt-5 text-3xl md:text-6xl font-semibold font-stretch-extra-expanded">
+            <span className="text-neutral-500">I&apos;m a</span>{" "}
+            <span className="bg-gradient-to-r from-[#FF0080] via-[#7928CA] to-[#0070F3] text-transparent bg-clip-text">
+              AI/ML, Web & Mobile App Developer.
+            </span>
+          </h1>
 
           {/* Book a call button */}
-          <BlurFade delay={0.25 * 3} inView>
-            <div className="flex flex-col md:flex-row gap-3 pt-10 items-center md:items-center">
-              <Link href={"#contact-me"}>
-                <InteractiveHoverButton>Book a call</InteractiveHoverButton>
-              </Link>
-
-              <TextAnimate
-                delay={0.25 * 4}
-                animation="blurInUp"
-                by="character"
-                once
-                className="text-base text-neutral-500 text-center md:text-left"
-              >
-                Feel free to explore my portfolio and reach out — I&apos;d love
-                to connect!
-              </TextAnimate>
-            </div>
-          </BlurFade>
+          <div className="flex flex-col md:flex-row gap-3 pt-10 items-center md:items-center">
+            <Link href={"#contact-me"}>
+              <InteractiveHoverButton>Book a call</InteractiveHoverButton>
+            </Link>
+            Feel free to explore my portfolio and reach out — I&apos;d love to
+            connect!
+          </div>
         </div>
       </div>
     </section>
