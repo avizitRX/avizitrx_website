a0e20f5bb846a143badf97e68680ee9c7a70d817
Avizit Roy <contact@avizitrx.com>
2025-03-21 12:05:53 +0600

    super speed optimization and cleanup

--- components/NextLevelSection.tsx
+++ components/NextLevelSection.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Image from "next/image";
 import { ContainerScroll } from "./ui/container-scroll-animation";
-import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
-import {
-  IconPalette,
-  IconTrendingUp,
-  IconSettings,
-  IconNumber123,
-} from "@tabler/icons-react";
-import { Compare } from "./ui/compare";
-import ConversionChart from "./ui/ConversionChart";
-import { AnimatedBeamUI } from "./ui/AnimatedBeamUI";
-import ProvenROI from "./ui/ProvenROI";
+// import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
+// import {
+//   IconPalette,
+//   IconTrendingUp,
+//   IconSettings,
+//   IconNumber123,
+// } from "@tabler/icons-react";
+// import { Compare } from "./ui/compare";
+// import ConversionChart from "./ui/ConversionChart";
+// import { AnimatedBeamUI } from "./ui/AnimatedBeamUI";
+// import ProvenROI from "./ui/ProvenROI";
 import { AuroraText } from "./magicui/aurora-text";
 import { BlurFade } from "./magicui/blur-fade";
 
@@ -29,21 +29,7 @@ const NextLevelSection = () => {
             </>
           }
         >
-          <div className="hidden md:block">
-            <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
-              {items.map((item, i) => (
-                <BentoGridItem
-                  key={i}
-                  title={item.title}
-                  description={item.description}
-                  header={item.header}
-                  className={item.className}
-                  icon={item.icon}
-                />
-              ))}
-            </BentoGrid>
-          </div>
-          <div className="block md:hidden">
+          <div>
             <Image
               src="/front-page/next-level.webp"
               height={720}
@@ -60,46 +46,46 @@ const NextLevelSection = () => {
 
 export default NextLevelSection;
 
-const items = [
-  {
-    title: "Increased Visibility & Traffic",
-    description:
-      "I optimize UX, speed, and design to increase sales & sign-ups.",
-    header: (
-      <Compare
-        firstImage="/front-page/dream_home_before.webp"
-        secondImage="/front-page/dream_home_after.webp"
-        firstImageClassName="object-cover  w-full"
-        secondImageClassname="object-cover  w-full"
-        className="w-full h-full rounded-[22px] md:rounded-lg"
-        slideMode="hover"
-        autoplay={true}
-      />
-    ),
-    className: "md:col-span-2",
-    icon: <IconPalette className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "Higher Conversions & Sales",
-    description:
-      "A high-traffic website/app is great—but what really matters is conversions.",
-    header: <ConversionChart />,
-    className: "md:col-span-1",
-    icon: <IconTrendingUp className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "Proven ROI & Success Stories",
-    description: "Delivering measurable results that matter.",
-    header: <ProvenROI />,
-    className: "md:col-span-1",
-    icon: <IconNumber123 className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "Business Growth Through Innovation",
-    description:
-      "Automate, optimize, and scale with AI, mobile apps, and custom tools.",
-    header: <AnimatedBeamUI />,
-    className: "md:col-span-2",
-    icon: <IconSettings className="h-4 w-4 text-neutral-500" />,
-  },
-];
+// const items = [
+//   {
+//     title: "Increased Visibility & Traffic",
+//     description:
+//       "I optimize UX, speed, and design to increase sales & sign-ups.",
+//     header: (
+//       <Compare
+//         firstImage="/front-page/dream_home_before.webp"
+//         secondImage="/front-page/dream_home_after.webp"
+//         firstImageClassName="object-cover  w-full"
+//         secondImageClassname="object-cover  w-full"
+//         className="w-full h-full rounded-[22px] md:rounded-lg"
+//         slideMode="hover"
+//         autoplay={true}
+//       />
+//     ),
+//     className: "md:col-span-2",
+//     icon: <IconPalette className="h-4 w-4 text-neutral-500" />,
+//   },
+//   {
+//     title: "Higher Conversions & Sales",
+//     description:
+//       "A high-traffic website/app is great—but what really matters is conversions.",
+//     header: <ConversionChart />,
+//     className: "md:col-span-1",
+//     icon: <IconTrendingUp className="h-4 w-4 text-neutral-500" />,
+//   },
+//   {
+//     title: "Proven ROI & Success Stories",
+//     description: "Delivering measurable results that matter.",
+//     header: <ProvenROI />,
+//     className: "md:col-span-1",
+//     icon: <IconNumber123 className="h-4 w-4 text-neutral-500" />,
+//   },
+//   {
+//     title: "Business Growth Through Innovation",
+//     description:
+//       "Automate, optimize, and scale with AI, mobile apps, and custom tools.",
+//     header: <AnimatedBeamUI />,
+//     className: "md:col-span-2",
+//     icon: <IconSettings className="h-4 w-4 text-neutral-500" />,
+//   },
+// ];
