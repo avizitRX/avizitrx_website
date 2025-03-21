4f35813f719c8b1398a8e7b13aac796e2ef8f078
Avizit Roy <contact@avizitrx.com>
2025-03-21 11:34:05 +0600

    more speed optimizatin and cleanup

--- components/ContactMeSection.tsx
+++ components/ContactMeSection.tsx
@@ -1,6 +1,4 @@
 "use client";
-import React, { useRef, useEffect } from "react";
-import { Confetti, ConfettiRef } from "./magicui/confetti";
 import { BlurFade } from "./magicui/blur-fade";
 import { AuroraText } from "./magicui/aurora-text";
 import dynamic from "next/dynamic";
@@ -9,53 +7,11 @@ const ContactForm = dynamic(() => import("./ui/ContactForm"), { ssr: false });
 const ContactInfo = dynamic(() => import("./ui/ContactInfo"), { ssr: false });
 
 const ContactMeSection = () => {
-  const confettiRef = useRef<ConfettiRef>(null);
-  const sectionRef = useRef<HTMLElement>(null);
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null); // Store timeout reference
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          // Clear any existing timeouts to prevent multiple triggers
-          if (timeoutRef.current) {
-            clearTimeout(timeoutRef.current);
-          }
-
-          // Set a timeout to delay confetti by 1 second
-          timeoutRef.current = setTimeout(() => {
-            confettiRef.current?.fire({});
-          }, 1000);
-        }
-      },
-      { threshold: 0.4 }
-    );
-
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current);
-    }
-
-    return () => {
-      if (sectionRef.current) {
-        observer.unobserve(sectionRef.current);
-      }
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
-      }
-    };
-  }, []);
-
   return (
     <section
       id="contact-me"
-      ref={sectionRef}
       className="relative flex items-center justify-center bg-black text-white px-6 overflow-hidden"
     >
-      {/* Confetti Effect */}
-      <Confetti
-        ref={confettiRef}
-        className="absolute left-0 top-0 w-full h-full"
-      />
       <div className="container text-center">
         {/* Section Heading */}
         <BlurFade delay={0.25} inView>
