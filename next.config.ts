79d7476852cbc169ff7f4686a194ecb23521cf51
Avizit Roy <contact@avizitrx.com>
2025-05-01 17:35:08 +0600

    fix: Sunrise Signal URL

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/sunrise-signal",
+        destination: "/projects/mobile-development/sunrise-signal",
+        permanent: true,
+      },
+    ];
+  },
   transpilePackages: ["next-mdx-remote"],
 };
 export default nextConfig;
