a7435b03decb13b433bdff3a58d108053def1c53
Avizit Roy <contact@avizitrx.com>
2025-09-24 21:12:53 +0600

    security: security headers add

--- next.config.ts
+++ next.config.ts
@@ -14,5 +14,37 @@ const nextConfig: NextConfig = {
     ];
   },
   transpilePackages: ["next-mdx-remote"],
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; object-src 'none'",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+        ],
+      },
+    ];
+  },
 };
+
 export default nextConfig;
