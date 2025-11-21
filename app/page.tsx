c879e55a17c7bc24f59813ac131bd2356e7a501d
Avizit Roy <contact@avizitrx.com>
2025-11-22 00:11:43 +0600

    ui: remove my work process section

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import ContactMeSection from "@/components/ContactMeSection";
 import CtaSection from "@/components/CtaSection";
 import FooterSection from "@/components/FooterSection";
 import HeroSection from "@/components/HeroSection";
-import MyWorkProcessSection from "@/components/MyWorkProcessSection";
 import Navbar from "@/components/Navbar";
 import NextLevelSection from "@/components/NextLevelSection";
 import PortfolioSection from "@/components/PortfolioSection";
@@ -18,7 +17,7 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       {/* <ServicesSection /> */}
-      <MyWorkProcessSection />
+      {/* <MyWorkProcessSection /> */}
       <PortfolioSection />
       <BlogSection />
       <NextLevelSection />
